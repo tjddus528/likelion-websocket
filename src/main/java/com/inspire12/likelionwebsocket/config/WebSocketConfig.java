1310a8a7ad12af8f1729220e616fb7a4dcefe222
inspire12 <ox4443@naver.com>
2025-03-24 19:03:24 +0900

    feat(websocket): cors, env 추가

--- src/main/java/com/inspire12/likelionwebsocket/config/WebSocketConfig.java
+++ src/main/java/com/inspire12/likelionwebsocket/config/WebSocketConfig.java
@@ -16,6 +16,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(new ChatWebSocketHandler(mapper),  "/ws")
-                .setAllowedOrigins("http://localhost:3000");
+//                .setAllowedOrigins("http://localhost:3000");
+                .setAllowedOrigins("*");
     }
 }
